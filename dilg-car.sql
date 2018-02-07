c0e291e90340ff7af3773621fdf63677e681c80f
Benedict Suarez <benedictsuarez888@gmail.com>
2018-02-07 21:21:21 +0800

    Added Tables
    I added the tables leave ledger, other info, plantilla, PDS Employee Response, Poisition, References, Trainings, and Voluntary Works.

--- dilg-car.sql
+++ dilg-car.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: dilg-car
 -- ------------------------------------------------------
--- Server version	5.7.11
+-- Server version	5.7.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,6 +42,286 @@ LOCK TABLES `applicationforleave` WRITE;
 /*!40000 ALTER TABLE `applicationforleave` DISABLE KEYS */;
 /*!40000 ALTER TABLE `applicationforleave` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `leave_ledger`
+--
+
+DROP TABLE IF EXISTS `leave_ledger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `leave_ledger` (
+  `ledger_id` varchar(45) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `app_id` varchar(45) DEFAULT NULL,
+  `leave_ledgercol` varchar(45) DEFAULT NULL,
+  `vacationAvailedLeaves` varchar(45) DEFAULT NULL,
+  `vacationNoOfAbsence` varchar(45) DEFAULT NULL,
+  `tardiness` varchar(45) DEFAULT NULL,
+  `vacationEarned` varchar(45) DEFAULT NULL,
+  `vacationBalance` varchar(45) DEFAULT NULL,
+  `vacationAbsenceWithPay` varchar(45) DEFAULT NULL,
+  `vacationAbscencesWithoutPay` varchar(45) DEFAULT NULL,
+  `sickAvailedLeaves` varchar(45) DEFAULT NULL,
+  `sickNoOfAbsenceTardiness` varchar(45) DEFAULT NULL,
+  `sickEarned` varchar(45) DEFAULT NULL,
+  `sickBalance` varchar(45) DEFAULT NULL,
+  `sickAbsenceWithPay` varchar(45) DEFAULT NULL,
+  `sickAbsencesWithoutPay` varchar(45) DEFAULT NULL,
+  `year` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ledger_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `leave_ledger`
+--
+
+LOCK TABLES `leave_ledger` WRITE;
+/*!40000 ALTER TABLE `leave_ledger` DISABLE KEYS */;
+/*!40000 ALTER TABLE `leave_ledger` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `other_info`
+--
+
+DROP TABLE IF EXISTS `other_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `other_info` (
+  `info_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `skillsAndHobbies` varchar(45) DEFAULT NULL,
+  `nonAcademicDistinction` varchar(45) DEFAULT NULL,
+  `membership` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`info_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `other_info`
+--
+
+LOCK TABLES `other_info` WRITE;
+/*!40000 ALTER TABLE `other_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `other_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pds_employee_response`
+--
+
+DROP TABLE IF EXISTS `pds_employee_response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pds_employee_response` (
+  `PdsEmployeeResponse_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `no34ResponseA` varchar(45) DEFAULT NULL,
+  `no34ResponseB` varchar(45) DEFAULT NULL,
+  `no34ResponseDetails` varchar(45) DEFAULT NULL,
+  `no35ResponseA` varchar(45) DEFAULT NULL,
+  `no35ResponseB` varchar(45) DEFAULT NULL,
+  `no35Details` varchar(45) DEFAULT NULL,
+  `no36Response` varchar(45) DEFAULT NULL,
+  `no36ResponseDetails` varchar(45) DEFAULT NULL,
+  `no37Response` varchar(45) DEFAULT NULL,
+  `no37Details` varchar(45) DEFAULT NULL,
+  `no38ResponseA` varchar(45) DEFAULT NULL,
+  `no38ResponseB` varchar(45) DEFAULT NULL,
+  `no38ResponseDetails` varchar(45) DEFAULT NULL,
+  `no39Response` varchar(45) DEFAULT NULL,
+  `no39ResponseDetails` varchar(45) DEFAULT NULL,
+  `no40ResponseA` varchar(45) DEFAULT NULL,
+  `no40ResponseADetails` varchar(45) DEFAULT NULL,
+  `no40ResponseB` varchar(45) DEFAULT NULL,
+  `no40ResponseBDetails` varchar(45) DEFAULT NULL,
+  `no40ResponseC` varchar(45) DEFAULT NULL,
+  `no40ResponseCDetails` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`PdsEmployeeResponse_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pds_employee_response`
+--
+
+LOCK TABLES `pds_employee_response` WRITE;
+/*!40000 ALTER TABLE `pds_employee_response` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pds_employee_response` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plantilla`
+--
+
+DROP TABLE IF EXISTS `plantilla`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `plantilla` (
+  `plantilla_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `position_id` varchar(45) DEFAULT NULL,
+  `division_id` varchar(45) DEFAULT NULL,
+  `itemNumber` varchar(45) DEFAULT NULL,
+  `positionTitle` varchar(45) DEFAULT NULL,
+  `salaryGrade` varchar(45) DEFAULT NULL,
+  `authorized_sal` varchar(45) DEFAULT NULL,
+  `actual_sal` varchar(45) DEFAULT NULL,
+  `step_increment` varchar(45) DEFAULT NULL,
+  `area_code` varchar(45) DEFAULT NULL,
+  `plantillacol` varchar(45) DEFAULT NULL,
+  `area_type` varchar(45) DEFAULT NULL,
+  `level` varchar(45) DEFAULT NULL,
+  `ppaAttribution` varchar(45) DEFAULT NULL,
+  `civil` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`plantilla_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plantilla`
+--
+
+LOCK TABLES `plantilla` WRITE;
+/*!40000 ALTER TABLE `plantilla` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plantilla` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position` (
+  `position_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`position_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `references`
+--
+
+DROP TABLE IF EXISTS `references`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `references` (
+  `reference_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `telephoneNo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`reference_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `references`
+--
+
+LOCK TABLES `references` WRITE;
+/*!40000 ALTER TABLE `references` DISABLE KEYS */;
+/*!40000 ALTER TABLE `references` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service_record`
+--
+
+DROP TABLE IF EXISTS `service_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `service_record` (
+  `service_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `startJobDate` varchar(45) DEFAULT NULL,
+  `endJobDate` varchar(45) DEFAULT NULL,
+  `designation` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `annualSalary` varchar(45) DEFAULT NULL,
+  `division` varchar(45) DEFAULT NULL,
+  `branch` varchar(45) DEFAULT NULL,
+  `remarks` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`service_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service_record`
+--
+
+LOCK TABLES `service_record` WRITE;
+/*!40000 ALTER TABLE `service_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `template`
+--
+
+DROP TABLE IF EXISTS `template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `template` (
+  `template_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `context` varchar(45) DEFAULT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  `salary` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`template_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `template`
+--
+
+LOCK TABLES `template` WRITE;
+/*!40000 ALTER TABLE `template` DISABLE KEYS */;
+/*!40000 ALTER TABLE `template` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voluntary_works`
+--
+
+DROP TABLE IF EXISTS `voluntary_works`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `voluntary_works` (
+  `voluntaryWork_id` int(11) NOT NULL,
+  `emp_id` varchar(45) DEFAULT NULL,
+  `nameAddressOfOrganization` varchar(45) DEFAULT NULL,
+  `from` varchar(45) DEFAULT NULL,
+  `to` varchar(45) DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`voluntaryWork_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voluntary_works`
+--
+
+LOCK TABLES `voluntary_works` WRITE;
+/*!40000 ALTER TABLE `voluntary_works` DISABLE KEYS */;
+/*!40000 ALTER TABLE `voluntary_works` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +332,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-25 12:16:30
+-- Dump completed on 2018-02-07 21:18:43
